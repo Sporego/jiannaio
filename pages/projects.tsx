dcb122ce4171eee10e8aa1b22fcc47f277286c9f
Sporego <contact@filbert.io>
2021-06-05 19:11:36 -0400

    created new route /projects

---
+++ pages/projects.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Navbar from '../components/Naxbar';
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Jianna Wang</title>
+			</Head>
+			<Navbar />
+		</div>
+	);
+}
