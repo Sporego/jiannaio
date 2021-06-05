6643ba6d9cd0b827d613c97745cf2cf6f742cc5f
Sporego <contact@filbert.io>
2021-06-05 19:10:17 -0400

    updated favicon to follow MDN standards

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,31 @@
-import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+	DocumentContext,
+	Html,
+	Head,
+	Main,
+	NextScript,
+} from 'next/document';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+	static async getInitialProps(ctx: DocumentContext) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
 
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="shortcut icon" href="/favicon.svg" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link rel='icon' type='image/svg+xml' href='favicon.svg' />
+					<link rel='icon' type='image/png' href='favicon.png' />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
 
-export default MyDocument
+export default MyDocument;
