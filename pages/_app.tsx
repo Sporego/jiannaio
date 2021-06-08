f3aa8997fa644630ef362c66574606b6c556d6f6
Sporego <contact@filbert.io>
2021-06-08 16:05:19 -0400

    added special global css imports for pdfjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,9 @@ import { ChakraProvider } from '@chakra-ui/react';
 // Next.js TSX Support
 import type { AppProps /*, AppContext */ } from 'next/app';
 
+//Speciall Global CSS Imports
+import '../styles/pdfjs.css'
+
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ChakraProvider>
